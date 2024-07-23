b33e4eb7412db2e3f58c8b91229282196db8bba8
maurilioSo <maurilio.sousil@gmail.com>
2024-07-22 22:26:00 -0300

    bad solution

--- Curso_Exceptions/src/aplication/Program.java
+++ Curso_Exceptions/src/aplication/Program.java
@@ -1,4 +1,4 @@
-package application;
+package aplication;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -35,15 +35,11 @@ public class Program {
             System.out.print("Check-out date (dd/MM/yyyy): ");
             checkOut = sdf.parse(sc.next());
 
-            Date now = new Date();
-            if (checkIn.before(now) || checkOut.before(now)) {
-                System.out.println("Error in reservation: Reservation dates for update must be future dates");
-            }
-            else if (!checkOut.after(checkIn)) {
-                System.out.println("Error in reservation: Check-out date must be after check-in date");
+            String error = reservation.updateDates(checkIn, checkOut);
+            if (error != null) {
+                System.out.println("Error in reservation: " + error);
             }
             else {
-                reservation.updateDates(checkIn, checkOut);
                 System.out.println("Reservation: " + reservation);
             }
         }
